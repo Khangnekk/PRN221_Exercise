bd5ba406c3f006b054e3a55e1d7fcac84096a88e
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-01-08 00:21:35 +0700

    Add and edit buttons are available

--- StudentInfo/Ex1/MainWindow.xaml.cs
+++ StudentInfo/Ex1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Ex1
 {
@@ -36,5 +37,50 @@ namespace Ex1
             }
         }
 
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student
+            {
+                Id = Convert.ToInt32(txtID.Text),
+                FullName = txtFullName.Text,
+                Major = txtMajor.Text,
+            };
+            StudentVM studentVM = (StudentVM)this.Resources["StudentVM"];
+            if (studentVM.Students.Any(s => student.Id == s.Id))
+            {
+                MessageBox.Show(" Id is unique! Please try again", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            else
+            {
+                studentVM.Students.Add(student);
+                CollectionViewSource.GetDefaultView(MyListbox.ItemsSource).Refresh();
+                MessageBox.Show("Successfully added new student", "Notfication", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student
+            {
+                Id = Convert.ToInt32(txtID.Text),
+                FullName = txtFullName.Text,
+                Major = txtMajor.Text,
+            };
+            StudentVM studentVM = (StudentVM)this.Resources["StudentVM"];
+            Student? currentStudent = studentVM.Students.FirstOrDefault(s => s.Id == student.Id);
+            if (currentStudent != null)
+            {
+                currentStudent.FullName = student.FullName;
+                currentStudent.Major = student.Major;
+                CollectionViewSource.GetDefaultView(MyListbox.ItemsSource).Refresh();
+                MessageBox.Show($"Successfully edited a student with Id{student.Id}", "Notfication", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show(" Maybe something wrong! Please try again", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+        }
     }
 }
