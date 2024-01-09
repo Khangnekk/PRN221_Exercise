57f809c99faf4e6303726b1cd6feb66d6696966f
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-01-10 01:04:49 +0700

    add appsettings.json file

--- StudentInfo/Ex2/Models/NorthwindContext.cs
+++ StudentInfo/Ex2/Models/NorthwindContext.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Ex2.Models
 {
@@ -29,8 +27,8 @@ namespace Ex2.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=MSI;database=Northwind;user=Khang;password=123;TrustServerCertificate=true");
+                var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("MyConnectionStrings"));
             }
         }
 
