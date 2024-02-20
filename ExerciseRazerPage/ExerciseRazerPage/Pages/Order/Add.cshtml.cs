f0fd8e0f9a073e0f78006198c982d7f890fe9ac4
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-21 00:28:14 +0700

    add order (design form)

--- ExerciseRazerPage/ExerciseRazerPage/Pages/Order/Add.cshtml.cs
+++ ExerciseRazerPage/ExerciseRazerPage/Pages/Order/Add.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
+using ExerciseRazerPage.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExerciseRazerPage.Pages.Order
 {
     public class AddModel : PageModel
     {
+        private readonly NorthwindContext _context;
+
+        public AddModel(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public List<Models.Employee> employees { get; set; }
+        public List<Models.Customer> customers { get; set; }
         public void OnGet()
         {
+            customers = _context.Customers.ToList();
+            employees = _context.Employees.ToList();
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
