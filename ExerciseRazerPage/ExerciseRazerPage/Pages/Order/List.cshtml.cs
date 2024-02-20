56a053392643687c1fd6950cb85779678ab5bc50
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-21 00:27:22 +0700

    list orders

--- ExerciseRazerPage/ExerciseRazerPage/Pages/Order/List.cshtml.cs
+++ ExerciseRazerPage/ExerciseRazerPage/Pages/Order/List.cshtml.cs
@@ -1,12 +1,29 @@
-using Microsoft.AspNetCore.Mvc;
+using ExerciseRazerPage.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExerciseRazerPage.Pages.Order
 {
-    public class ListModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class ListModel : PageModel
+	{
+		private readonly NorthwindContext _context;
+
+		public ListModel(NorthwindContext context)
+		{
+			_context = context;
+		}
+		public List<Models.Order> Orders { get; set; }
+		public void OnGet()
+		{
+			Orders = _context.Orders
+				.Include(c => c.Customer)
+				.Include(e => e.Employee)
+				.ToList();
+		}
+
+		public void OnGetAdd()
+		{
+			RedirectToPage("/Add");
+		}
+	}
 }
