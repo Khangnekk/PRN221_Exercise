c9ef8ea87c460db164141e4691fb2fb723e7ce92
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-26 01:51:39 +0700

    add filter function

--- ExerciseRazerPage/ExerciseRazerPage/Pages/Order/Add.cshtml.cs
+++ ExerciseRazerPage/ExerciseRazerPage/Pages/Order/Add.cshtml.cs
@@ -1,4 +1,5 @@
 using ExerciseRazerPage.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExerciseRazerPage.Pages.Order
@@ -13,6 +14,20 @@ namespace ExerciseRazerPage.Pages.Order
         }
         public List<Models.Employee> employees { get; set; }
         public List<Models.Customer> customers { get; set; }
+        #region
+        [BindProperty]
+        public int? EmployeeID { get; set; }
+        [BindProperty]
+        public string? CustomerID { get; set; }
+        [BindProperty]
+        public DateTime? OrderDate { get; set; }
+        [BindProperty]
+        public string? ShipAddress { get; set; }
+        [BindProperty]
+        public string? ShipCountry { get; set; }
+        [BindProperty]
+        public string? Message { get; set; }
+        #endregion
         public void OnGet()
         {
             customers = _context.Customers.ToList();
@@ -21,7 +36,15 @@ namespace ExerciseRazerPage.Pages.Order
 
         public void OnPost()
         {
-
+            Models.Order o = new Models.Order();
+            o.EmployeeId = EmployeeID;
+            o.CustomerId = CustomerID;
+            o.OrderDate = OrderDate;
+            o.ShipCountry = ShipCountry;
+            o.ShipAddress = ShipAddress;
+            _context.Orders.Add(o);
+            _context.SaveChanges();
+            Message = $"Add Order Successfully";
         }
     }
 }
--- ExerciseRazerPage/ExerciseRazerPage/Pages/Order/List.cshtml.cs
+++ ExerciseRazerPage/ExerciseRazerPage/Pages/Order/List.cshtml.cs
@@ -1,4 +1,5 @@
 using ExerciseRazerPage.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,17 +14,60 @@ namespace ExerciseRazerPage.Pages.Order
 			_context = context;
 		}
 		public List<Models.Order> Orders { get; set; }
+		public List<Models.Employee> employees { get; set; }
+		public List<Models.Customer> customers { get; set; }
+		#region
+		[BindProperty]
+		public int? EmployeeID { get; set; }
+		[BindProperty]
+		public string? CustomerID { get; set; }
+		[BindProperty]
+		public DateTime? FromDate { get; set; }
+		[BindProperty]
+		public DateTime? ToDate { get; set; }
+		[BindProperty]
+		public string SortBy { get; set; }
+		[BindProperty]
+		public string? Message { get; set; }
+		#endregion
 		public void OnGet()
 		{
 			Orders = _context.Orders
 				.Include(c => c.Customer)
 				.Include(e => e.Employee)
 				.ToList();
+			customers = _context.Customers.ToList();
+			employees = _context.Employees.ToList();
 		}
 
 		public void OnGetAdd()
 		{
 			RedirectToPage("/Add");
+		}
+
+		public void OnPost()
+		{
+			customers = _context.Customers.ToList();
+			employees = _context.Employees.ToList();
+
+			Orders = _context.Orders
+				.Include(c => c.Customer)
+				.Include(e => e.Employee)
+				.Where(e => EmployeeID == -1 || e.EmployeeId == EmployeeID)
+				.Where(e => CustomerID == "-1" || e.CustomerId == CustomerID)
+				.Where(e => e.OrderDate >= FromDate && e.OrderDate <= ToDate)
+				.ToList();
+			switch (SortBy)
+			{
+				case "OrderDate":
+					Orders = Orders.OrderBy(o => o.OrderDate).ToList();
+					break;
+				case "Freight":
+					Orders = Orders.OrderBy(o => o.Freight).ToList();
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
