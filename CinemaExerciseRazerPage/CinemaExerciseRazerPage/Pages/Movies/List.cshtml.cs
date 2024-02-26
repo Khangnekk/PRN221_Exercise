7130f14087410a5994a2884412ce338d36838b80
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-27 00:50:39 +0700

    create table

--- CinemaExerciseRazerPage/CinemaExerciseRazerPage/Pages/Movies/List.cshtml.cs
+++ CinemaExerciseRazerPage/CinemaExerciseRazerPage/Pages/Movies/List.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
+using CinemaExerciseRazerPage.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CinemaExerciseRazerPage.Pages.Movies
 {
-    public class ListModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class ListModel : PageModel
+	{
+		private readonly CinemaContext _context;
+
+		public ListModel(CinemaContext context)
+		{
+			_context = context;
+		}
+		public List<Models.Movie> Movies { get; set; }
+		public List<Models.Star> Stars { get; set; }
+		public void OnGet()
+		{
+			Movies = _context.Movies.ToList();
+			Stars = _context.Stars.ToList();
+		}
+	}
 }
