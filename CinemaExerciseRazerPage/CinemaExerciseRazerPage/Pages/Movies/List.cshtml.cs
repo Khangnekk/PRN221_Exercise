bc0bd53cccd1978a7e2e4032d71103c2a9d1b40a
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-02-26 09:37:24 +0700

    init razer page

---
+++ CinemaExerciseRazerPage/CinemaExerciseRazerPage/Pages/Movies/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaExerciseRazerPage.Pages.Movies
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
