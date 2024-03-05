fa933b98d50f9c3df3f49b8d945853db7e98178f
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-03-06 03:41:52 +0700

    init Lab2n3

---
+++ Lab2N3/Lab2N3/Program.cs
@@ -0,0 +1,13 @@
+using Lab2N3.Models;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<NorthwindContext>(
+	opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionStrings")));
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
+app.Run();
